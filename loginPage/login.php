4ce4d355b51dbdbb34c9912cb32e6955cafa9c05
jandreagiralao <60372091+jandreagiralao@users.noreply.github.com>
2020-10-29 17:45:26 +0800

    Updated CSS

--- loginPage/login.php
+++ loginPage/login.php
@@ -1,4 +1,7 @@
-<?php include('login_method.php') ?>
+<?php include('login_method.php'); 
+	//remove this later
+	unset($_SESSION['user_type']);
+?>
 
 <!DOCTYPE html>
 <html>
@@ -6,31 +9,37 @@
 	<title>PhilCafe - Login</title>
 	<script src="https://kit.fontawesome.com/58872a6613.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../assets/css/login-style.css">
 </head>
 <body>
 	<div id="container">
-		<div class="left-login"></div>
+		<div class="left-login">
+			<img src="../assets/img/login-bg.jpg">
+		</div>
 
 		<div class="right-login">
-				<a href="../homePage/home.php" class="home">
-					<i class="far fa-arrow-alt-circle-left fa-2x"></i>
-					Back to Home
+			<div class="return">
+				<a href="../homePage/home.php" class="home" >
+					<i class="far fa-arrow-alt-circle-left fa-lg"></i>&nbsp;
+					<span style="font-size: 18px;">Back to Home</span>
 				</a>
-			<div id="login-form">
-				<br><p><strong>Login</strong></p>
-				<form method="post" action="login.php">
-					<?php echo display_error(); 
-						unset($errors);
-					?>
+			</div>
 
+			<div id="login-form">
+				<form method="post" action="login.php">
+					<span style="font-size: 24px;"><strong>Login</strong></span><br>
+					<span style="color: red;"><small><?php echo display_error(); 
+						unset($errors);
+					?></small></span>
+					<br>
 					<input type="email" class="form-control" name="email-cred" placeholder="Email Address"required><br>
 					<input type="password" class="form-control" name="password-cred" placeholder="Password"required><br>
-					<button type="submit" name="login-button" class="btn btn-success">LOGIN</button>
-				</form>.
+					<button type="submit" name="login-button" class="btn btn-success pull-right">LOGIN</button>
+				</form>
 
-				<hr class="rounded-border"><br>
+				<hr class="rounded-border">
 				<p>Don't have an account?</p>
-				<form method="post" action="registrationPage/">
+				<form method="post" action="registrationPage/" class="reg-button">
 					<button type="submit" name="submit-button" class="btn btn-warning">CREATE A NEW ACCOUNT</button>
 				</form>
 			</div>
--- loginPage/login_method.php
+++ loginPage/login_method.php
@@ -12,54 +12,7 @@
 
 	if(isset($_POST['login-button']) && !isLoggedIn())
 	{
-		$email = filter_var($_POST['email-cred'], FILTER_SANITIZE_EMAIL);
-		$password = $_POST['password-cred'];
-
-		if(empty($email))
-		{
-			array_push($errors, "Input Email");
-		}
-
-		if(empty($password))
-		{
-			array_push($errors, "Input Password");
-		}
-
-		if(count($errors) == 0)
-		{
-			//hash password
-			/*				*/
-			
-			$query = "SELECT * FROM LGU WHERE LGUemail = '$email' AND LGUpassword = '$password' LIMIT 1";
-			$results = mysqli_query($db, $query);
-
-			// user is admin
-			if(mysqli_num_rows($results) == 1)
-			{
-				$_SESSION['user_type'] = 'admin';
-				header('Location: ../admin.php');
-			}
-
-			//search in customers table
-			else
-			{
-				$query = "SELECT * FROM customers WHERE customerEmail = '$email' AND password = '$password' LIMIT 1";
-				$results = mysqli_query($db, $query);
-
-				if(mysqli_num_rows($results) == 1)
-				{
-					$_SESSION['user_type'] = 'customer';
-					echo $_SESSION['user_type'];
-					header('Location: ../customer.php');
-				}
-
-				//else, user does not exist
-				else
-				{
-					array_push($errors, "Email and password combination does not match.");
-				}
-			}
-		}
+		login();
 	}
 
 	function display_error()
@@ -77,10 +30,65 @@
 
 	function isLoggedIn()
 	{
-		if (isset($_SESSION['user'])) {
+		if (isset($_SESSION['user_type'])) {
 			return true;
 		}else{
 			return false;
+		}
+	}
+
+	function login()
+	{
+		global $db, $errors;
+
+		$email = filter_var($_POST['email-cred'], FILTER_SANITIZE_EMAIL);
+		$password = $_POST['password-cred'];
+
+		if(empty($email))
+		{
+			array_push($errors, "Input Email");
+		}
+
+		if(empty($password))
+		{
+			array_push($errors, "Input Password");
+		}
+
+		//hash password
+		/*				*/
+
+		if(count($errors) == 0)
+		{
+			
+
+			$query = "SELECT * FROM LGU WHERE LGUemail = '$email' AND LGUpassword = '$password' LIMIT 1";
+			$results = mysqli_query($db, $query);
+
+			// user is admin
+			if(mysqli_num_rows($results) == 1)
+			{
+				//$_SESSION['user_type'] = 'admin';
+				header('Location: ../admin.php');
+			}
+
+			//search in customers table
+			else
+			{
+				$query = "SELECT * FROM customers WHERE customerEmail = '$email' AND password = '$password' LIMIT 1";
+				$results = mysqli_query($db, $query);
+
+				if(mysqli_num_rows($results) == 1)
+				{
+					//$_SESSION['user_type'] = 'customer';	
+					header('Location: ../customer.php');
+				}
+
+				//else, user does not exist
+				else
+				{
+					array_push($errors, "Email and password combination does not match.");
+				}
+			}
 		}
 	}
 ?>
