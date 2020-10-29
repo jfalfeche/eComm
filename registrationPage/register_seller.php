9a1a98e47c902b22b6ce69fba13a15d2cf627784
Kaila Malabanan <kmmalabanan@up.edu.ph>
2020-10-29 14:15:58 +0800

    Update register_seller.php

--- registrationPage/register_seller.php
+++ registrationPage/register_seller.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Buyer Registration</title>
+    <title>Seller Registration</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
