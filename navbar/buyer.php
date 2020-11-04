c1b2f5c0c897fbd9ae1fcda961c21d1de6b4327a
Kaila Malabanan <kmmalabanan@up.edu.ph>
2020-11-05 04:56:09 +0800

    created buyer profile page

---
+++ navbar/buyer.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    $url = "https://";
+else
+    $url = "http://";
+// Append the host(domain name, ip) to the URL.   
+$url .= $_SERVER['HTTP_HOST'];
+
+// Append the requested resource location to the URL   
+$url .= $_SERVER['REQUEST_URI'];
+?>
+
+<a class="col-md-1 <?php if (strpos($url, 'http://localhost/eComm/homePage/home.php') == 'true') echo 'nav-active' ?>" href="../homePage/home.php">Home</a>
+<a class="col-md-1" href="">About</a>
+<a class="col-md-1 <?php if (strpos($url, 'http://localhost/eComm/products/product.php') == 'true') echo 'nav-active' ?>" href="../products/product.php">Products</a>
+<a class="col-md-1 <?php if (strpos($url, 'http://localhost/eComm/phpmailer/contact.php') == 'true') echo 'nav-active' ?>" href="../phpmailer/contact.php">Contact</a>
+<a class="col-md-1" href="">My Cart</a>
+<a class="col-md-1" href="<?php if (strpos($url, 'http://localhost/eComm/profiles/prof_buyer.php') == 'true') echo 'nav-active' ?>">My Profile</a>
+<a class="col-md-1" href="">Logout</a>
