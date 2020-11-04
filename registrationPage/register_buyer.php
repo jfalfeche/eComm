521fd5ce8363f80abb48990ec0b71a6cde4d0b0c
Kaila Malabanan <kmmalabanan@up.edu.ph>
2020-11-04 14:22:47 +0800

    added back-end for register_buyer and register_seller

--- registrationPage/register_buyer.php
+++ registrationPage/register_buyer.php
@@ -1,113 +1,136 @@
-<!DOCTYPE html>
-<html>
+<?php include('method_buyer.php');?>
 
-<head>
-    <title>Buyer Registration</title>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="reg.css">
-    <link rel="stylesheet" href="../navbar/nav.css">
-    <link rel="stylesheet" href="../footer/footer.css">
-</head>
+   <!DOCTYPE html>
+   <html>
 
-<body>
-    <!--NAV-->
-    <nav class="nav guest">
-        <div class="col-md-1">
-            <div class="logo">
-                <h1>LOGO</h1>
-            </div>
-        </div>
-        <div class="col-md-6"></div>
-        <?php include '../navbar/guest.php' ?>
-    </nav>
-    <!--END NAV-->
+   <head>
+       <title>Buyer Registration</title>
+       <meta charset="UTF-8" />
+       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+       <link rel="stylesheet" href="reg.css">
+       <link rel="stylesheet" href="../navbar/nav.css">
+       <link rel="stylesheet" href="../footer/footer.css">
+   </head>
 
-    <div class="header">
-        <div class="row">
-            <a class="back" href=""><img src="../media/back1.svg" alt=""></a>
-            <h5>Back to Login</h5>
-        </div>
-        <h2 class="text-center">Register as PhilCafe Customer</h2>
-        <hr class="solid">
-    </div>
+   <body>
+       <!--NAV-->
+       <nav class="nav guest">
+           <div class="col-md-1">
+               <div class="logo">
+                   <h1>LOGO</h1>
+               </div>
+           </div>
+           <div class="col-md-6"></div>
+           <?php include '../navbar/guest.php' ?>
+       </nav>
+       <!--END NAV-->
 
-    <!--REGISTRATION FORM-->
-    <div class="signup-form">
-        <form action="" method="post">
-            <div class="form-group">
-                <p>Full Name</p>
-                <div class="row">
-                    <div class="col"><input type="text" class="form-control" name="firstName" placeholder="First Name" required="required"></div>
-                    <div class="col"><input type="text" class="form-control" name="middleName" placeholder="Middle Name" required="required"></div>
-                    <div class="col"><input type="text" class="form-control" name="lastName" placeholder="Last Name" required="required"></div>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="row">
-                    <div class="col">
-                        <p>Username</p><input type="text" class="form-control" name="username" placeholder="Buyer2020" required="required">
-                    </div>
-                    <div class="col">
-                        <p>Phone Number</p><input type="text" class="form-control" name="contactNumber" placeholder="0910*******" required="required">
-                    </div>
-                </div>
-            </div>
+       <!--       <a class="" href=""><img src=" ../media/back1.svg"><h5>Back to Login</h5></a> -->
+       <h2 class="text-center">Register as PhilCafe Customer</h2>
+       <hr class="solid">
 
-            <div class="form-group">
-                <p>Delivery Address</p>
-                <input type="text" class="form-control" name="permanentAddress" placeholder="House no., Street no., Barangay, City" required="required">
-            </div>
-            <div class="form-group">
-                <div class="row">
-                    <div class="col">
-                        <p>Gender</p><label class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="gender" value="female">
-                            <span class="form-check-label"> Female</span>
-                        </label>
-                        <label class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="gender" value="male">
-                            <span class="form-check-label"> Male </span>
-                        </label>
-                    </div>
-                    <div class="col">
-                        <p>Birthday</p><input type="date" class="form-control" name="birthday" placeholder="Phone Number" required="required">
-                    </div>
-                </div>
-            </div>
-            <div class="form-group">
+       <!--REGISTRATION FORM-->
+       <div class="signup-form">
+           <form method='post' action=''>
 
-                <div class="row">
-                    <div class="col">
-                        <p>Email Address</p><input type="text" class="form-control" name="customerEmail" placeholder="sample_email2020@gmail.com" required="required">
-                    </div>
-                    <div class="col">
-                        <p>Confirm Email Address</p><input type="text" class="form-control" name="customerEmail" placeholder="sample_email2020@gmail.com" required="required">
-                    </div>
-                </div>
-            </div>
-            <div class="form-group">
+               <?php
+                // Display Error message
+                $error_message;
+                $success_message;
 
-                <div class="row">
-                    <div class="col">
-                        <p>Password</p><input type="password" class="form-control" name="password" placeholder="*minimum of 8 characters" required="required">
-                    </div>
-                    <div class="col">
-                        <p>Confirm Password</p><input type="password" class="form-control" name="password" placeholder="*minimum of 8 characters" required="required">
-                    </div>
-                </div>
-            </div>
-        </form>
-        <button type="submit" name="submit-button" class="btn btn-success btn-lg btn-block">Create Account</button>
-    </div>
+                if (!empty($error_message)) {
+                ?>
+                   <div class="alert alert-danger">
+                       <strong>Error!</strong> <?= $error_message ?>
+                   </div>
 
-    <!--END OF REGFORM-->
+               <?php
+                }
+                ?>
 
-    <!--FOOTER-->
-    <?php include '../footer/shortfooter.php'; ?>
-    <!--END FOOTER-->
-</body>
+               <?php
+                // Display Success message
+                if (!empty($success_message)) {
+                ?>
+                   <div class="alert alert-success">
+                       <strong>Success!</strong> <?= $success_message ?>
+                   </div>
 
-</html>
+               <?php
+                }
+                ?>
+
+               <div class="form-group">
+                   <p>Full Name</p>
+                   <div class="row">
+                       <div class="col"><input type="text" class="form-control" name="firstName" placeholder="First Name" required="required"></div>
+                       <div class="col"><input type="text" class="form-control" name="middleName" placeholder="Middle Name" required="required"></div>
+                       <div class="col"><input type="text" class="form-control" name="lastName" placeholder="Last Name" required="required"></div>
+                   </div>
+               </div>
+
+               <div class="form-group">
+                   <div class="row">
+                       <div class="col">
+                           <p>Phone Number</p><input type="text" class="form-control" name="contactNumber" placeholder="09*******" required="required">
+                       </div>
+                       <div class="col">
+                           <p>Delivery Address</p><textarea class="form-control" name="permanentAddress" placeholder="House no., Street no., Barangay, City" rows="5" required="required"></textarea>
+                       </div>
+                   </div>
+               </div>
+
+               <div class="form-group">
+                   <div class="row">
+                       <div class="col">
+                           <p>Birthday</p><input type="date" class="form-control" name="birthday" required="required">
+                       </div>
+                       <div class="col">
+                           <p>Gender</p><label class="form-check form-check-inline">
+                               <input class="form-check-input" type="radio" name="gender" value="female" required>
+                               <span class="form-check-label"> Female</span>
+                           </label>
+                           <label class="form-check form-check-inline">
+                               <input class="form-check-input" type="radio" name="gender" value="male">
+                               <span class="form-check-label"> Male </span>
+                           </label>
+                       </div>
+
+                   </div>
+               </div>
+               <div class="form-group">
+
+                   <div class="row">
+                       <div class="col">
+                           <p>Email Address</p><input type="text" class="form-control" name="customerEmail" placeholder="sample_email2020@gmail.com" required="required">
+                       </div>
+                       <div class="col">
+                           <p>Confirm Email Address</p><input type="text" class="form-control" name="confirmEmail" placeholder="sample_email2020@gmail.com" required="required">
+                       </div>
+                   </div>
+               </div>
+               <div class="form-group">
+
+                   <div class="row">
+                       <div class="col">
+                           <p>Password</p><input type="password" class="form-control" name="password" placeholder="*minimum of 8 characters" required="required">
+                       </div>
+                       <div class="col">
+                           <p>Confirm Password</p><input type="password" class="form-control" name="confirmPassword" placeholder="*minimum of 8 characters" required="required" minlength="8">
+                       </div>
+                   </div>
+               </div>
+               <button type="submit" name="submit" class="btn btn-success btn-lg btn-block">Create Account</button>
+           </form>
+       </div>
+
+       <!--END OF REGFORM-->
+
+       <!--FOOTER-->
+       <?php include '../footer/shortfooter.php'; ?>
+       <!--END FOOTER-->
+   </body>
+
+   </html>
