2c1a97e4b9e9ef28be6953c65f82fa56353e8745
Kaila Jeanne Malabanan <60372357+kjeanne13@users.noreply.github.com>
2021-01-10 22:15:51 +0800

    created order summary page

---
+++ checkout/checkoutSummary.php
@@ -0,0 +1,191 @@
+<?php include 'checkoutSummary_method.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Order Summary</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/58872a6613.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
+    <link rel="stylesheet" href="../navbar/nav.css">
+    <link rel="stylesheet" href="../footer/footer.css">
+    <link rel="stylesheet" href="./checkout.css">
+</head>
+
+<body>
+<!--NAV-->
+ <nav class="nav buyer">
+        <div class="col-md-3">
+            <div class="logo">
+                <img class="imglogo" src="../assets/img/philcafe.png" alt="">
+                <h1 class="logotitle">PhilCafe</h1>
+            </div>
+        </div>
+        <div class="col-md-2"></div>
+        <?php include '../navbar/buyer.php' ?>
+    </nav>
+<!--END NAV-->
+
+<div id="container">
+
+    <div class="column left">
+        <div class="header">
+            <a href="../profiles/pendingOrders.php"><i class="fa fa-arrow-circle-left fa-2x" style="color: #200E32;"></i></a>
+            <h1 class="underline">SHIPPING INFORMATION</h1>
+        </div>
+
+        <table class="table table-bordered">
+
+        <?php
+            $result = $conn->query($info);
+            if (mysqli_num_rows($result) == 1) {
+                $row = $result->fetch_assoc();
+        ?>
+
+            <tbody>
+                <th scope="col" colspan="2">Personal Details</th>
+                <tr><td colspan="2"><?php echo get_customer_name($row['userID']); ?></td></tr>
+                <tr>
+                    <td><?php echo $row["contactNumber"] ?></td>
+                    <td><?php echo $row["customerEmail"] ?></td>
+                </tr>
+
+        <?php
+            } $result = $conn->query($info2);
+                if (mysqli_num_rows($result) == 1) {
+                $row = $result->fetch_assoc();
+        ?>
+
+                <tr><th scope="col" colspan="2">Address Details</th></tr>
+                <tr><td colspan="2"><?php echo $row["shippingAddress"] ?></td></tr>
+                
+                <tr><th scope="col" colspan="2">Payment Method</th></tr>
+                <tr><td colspan="2"><?php echo $row["paymentMethod"] ?></td></tr>
+                
+                <tr><th scope="col" colspan="2">Message to Seller</th></tr>
+                <tr><td colspan="2"><?php echo $row["message"] ?></td> </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <?php 
+        } $result = $conn->query($total);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+    ?>
+
+    <div class="column right">
+        <div class="header"><h1 class="underline">ORDER SUMMARY</h1></div>
+        <table class="table table-borderless summary">
+        <tbody>
+            <tr>
+                <th>ORDER NUMBER</th>
+                <td></td>
+                <th><?php echo sprintf('%08d', $orderNo) ?></th>
+            </tr>
+            <tr>
+                <th>ORDER STATUS</th>
+                <td></td>
+                <td><span style="color: <?php echo get_status_color($row['status']) ?>;">
+	                    <strong><?php get_status_name($row['status']); ?></strong>
+	                </span>
+                </td>
+                <td></td>
+            </tr>
+
+            <?php }
+                $subtotal = 0;
+                $shippingFee = 0;
+
+                $result = $conn->query($items);
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = $result->fetch_assoc()) {
+
+                $subtotal += get_item_total($row['price'], $row['quantity']); 
+            ?>
+
+            <tr>
+                <td>
+                    <div style="text-align: center;"><?php echo $row['productName'] ?></div>
+                </td>
+                <td>
+                    <div style="text-align: center;"><?php echo $row['quantity'] ?></div>
+                </td>
+                <td>
+	                <div style="text-align: right;">PHP <?php echo number_format(get_item_total($row['price'], $row['quantity']), 2, '.', ' ');?></div>
+	            </td>
+            </tr>
+
+            <?php
+                }
+            }
+            ?>
+
+        </tbody>
+        </table><br>
+
+        <table class="table summary">
+        <tbody>
+            <tr>
+		        <td></td><td></td>
+                <td>Subtotal</td>
+		        <td>
+		            <div style="text-align: right;">
+		            	PHP <?php echo number_format($subtotal, 2, '.', ' '); ?>
+		            </div>
+                </td>
+            </tr>
+
+            <?php
+		        $result = $conn->query($total);
+		            if ($result->num_rows > 0) {
+		                $row = $result->fetch_assoc();
+		    ?>
+
+            <tr>
+		        <td></td><td></td>
+                <td>Shipping Fee</td>
+		        <td>
+		            <div style="text-align: right;">
+                        PHP <?php echo number_format($shippingFee, 2, '.', ' '); ?>
+                    </div>
+		        </td>
+            </tr>
+
+            <tr>
+                <td></td><td></td>
+                <th>TOTAL</th>
+		        <td>
+                    <div style="text-align: right;">
+		                <b>PHP <?php echo number_format($row['totalAmount'], 2, '.', ' ') ?></b>
+		            </div>
+		        </td>
+            </tr>
+
+            <?php
+                }
+            ?>
+
+        </tbody>
+        </table>
+
+        <!--BUTTONS-->
+        <div class="center">
+            <div class="buttons">
+                <button type="button" onclick="location.href='../profiles/cartSummary.php'" style="background-color:#2D9CDB;" class="btn btn-one btn-success">DETAILED ORDER SUMMARY</button>
+                <button type="submit" name="submit" class="btn btn-two btn-success btn-lg">Cancel Order</button>
+            </div>
+        </div>
+        <!--END BUTTONS-->
+    </div>
+
+</div>
+
+<!-- Footer -->
+    <?php include '../footer/shortfooter.php' ?>
+<!-- Footer -->
+</body>
+</html>
---
+++ checkout/checkoutSummary_method.php
@@ -0,0 +1,81 @@
+<?php
+	if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    if(isset($_SESSION['userID']) && (isset($_GET['orderNo']) && is_numeric($_GET['orderNo']))) {
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "philcafe";
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            //get buyerID
+            $buyerID = $userID = $_SESSION['userID'];
+            $orderNo =  filter_var($_GET['orderNo'], FILTER_SANITIZE_NUMBER_INT);
+
+            $info = "SELECT * FROM `customers` WHERE userID=$userID LIMIT 1";
+            $info2 = "SELECT shippingAddress, paymentMethod, message FROM `order` WHERE orderNo = '$orderNo'";
+            $items = "SELECT productdetail.quantity, product.productName, product.price, sellers.storeName FROM `productDetail` 
+			INNER JOIN `product` ON productdetail.productID = product.productID 
+			INNER JOIN `sellers` ON product.seller = sellers.sellerID
+            WHERE orderNo  = '$orderNo' AND buyerID = '$buyerID'";
+            
+            $total = "SELECT shippingFee, totalAmount, status FROM `order` WHERE orderNo = '$orderNo'";
+    
+    
+        if (isset($_POST['submit'])) {
+            
+        }
+
+    } else
+        header("Location: ../loginPage/login.php");
+?>
+
+<?php
+    function get_item_total($price, $quantity) {
+        return $price * $quantity;
+    }
+
+    function get_customer_name($userID){
+        global $conn;
+
+        $sql = "SELECT firstName, middleName, lastName FROM `customers` WHERE userID=$userID LIMIT 1";
+        $result = $conn->query($sql);
+
+        if (mysqli_num_rows($result) == 1) {
+            $row = $result->fetch_assoc();
+            $name = $row['firstName'] . " " . $row['middleName'] . " " . $row['lastName'];
+            echo ($name);
+        }
+    }
+
+    function get_status_name($status_id) {
+		global $conn;
+		$sql = "SELECT name from `orderStatus` WHERE orderStatusID=$status_id LIMIT 1";
+    	$result = $conn->query($sql);
+
+    	if(mysqli_num_rows($result) == 1)
+    	{
+    		$row = $result->fetch_assoc();
+    		echo strtoupper($row['name']);
+    	}
+    }
+    
+    function get_status_color($value){
+		if($value == "1")
+			return "#FF0000";
+		else if($value == "2")
+			return "#00FFD9";
+		else if($value == "3")
+			return "#0057FF";
+		else if($value == "4")
+			return "#FFF500";
+		else if($value == "5")
+			return "#0A5900";
+		else if($value == "6")
+			return "#967E69";
+
+	}
+?>
